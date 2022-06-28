754ca9f5f9466ec3dd4c4e2f5a87ebab970b031a
tayk6ix <89669855+tayk6ix@users.noreply.github.com>
2022-06-28 17:44:44 -0500

    Added PhP

---
+++ main.php
@@ -0,0 +1,18 @@
+<?php
+//get data from form
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$to = "jl435063@hotmail.com";
+$subject = $_POST['subject'];
+$txt = "Name = " . $first_name . $last_name . "\r\n  Email = " . $email . "\r\n  Phone # = " . $phone . "\r\n Message =" . $message;
+$headers = "From: noreply@jnjconstructionllc.net" . "\r\n" .
+    "CC: somebodyelse@example.com";
+if ($email != null) {
+    mail($to, $subject, $txt, $headers);
+    echo '<script language="javascript">';
+    echo 'alert("message successfully sent")';
+    echo '</script>';
+}
