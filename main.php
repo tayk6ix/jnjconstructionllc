33aa53f6ee8fc682334a862ad40c9b91d5f88fb6
tayk6ix <89669855+tayk6ix@users.noreply.github.com>
2022-06-28 18:11:48 -0500

    php update

--- main.php
+++ main.php
@@ -5,12 +5,27 @@ $last_name = $_POST['last_name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
-$to = "jl435063@hotmail.com";
 $subject = $_POST['subject'];
-$txt = "Name = " . $first_name . $last_name . "\r\n  Email = " . $email . "\r\n  Phone # = " . $phone . "\r\n Message =" . $message;
-$headers = "From: noreply@jnjconstructionllc.net" . "\r\n" .
-    "CC: somebodyelse@example.com";
-if (isset($_POST['submit']) && $email != null) {
-    mail($to, $subject, $txt, $headers);
-    header("Location:index.html?mailsend");
-}
+
+$to = "jl435063@hotmail.com";
+$headers = "From: " . $email . "\r\n Subject: " . $subject;
+$txt = "Name = " . $first_name . $last_name . "\r\n  Phone # = " . $phone . "\r\n Message =" . $message;
+
+mail($to, $subject, $txt, $headers) or die("Error!");
+
+echo '
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <title>J&J Construction, LLC.</title>
+    </head>
+<body>
+    <h1>Thankyou</h1>
+</body>
+</html>
+';
